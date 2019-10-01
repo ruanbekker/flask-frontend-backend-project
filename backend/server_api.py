b2712d5102f3f71a2e84033bccfc0ca02420b0e9
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-10-02 00:10:31 +0200

    add api code

--- backend/server_api.py
+++ backend/server_api.py
@@ -1,1 +1,19 @@
+from flask import Flask, request, jsonify, render_template
+import json
+import requests
 
+df = open('data.json', 'r')
+data = json.loads(df.read())
+
+app = Flask(__name__)
+
+@app.route('/api/v1/term/<keyword>')
+def search(keyword):
+    results = []
+    for each in data:
+        if keyword in each['project_text']:
+            results.append(each)
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(port=5001)
