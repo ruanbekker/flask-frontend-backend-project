dd362470ac90596cf8a6fbd5b31513a6c223deed
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-10-02 00:12:44 +0200

    add frontend code

--- frontend/server_ui.py
+++ frontend/server_ui.py
@@ -1,1 +1,16 @@
+from flask import Flask, request, render_template
+import requests
 
+app = Flask(__name__)
+
+@app.route('/')
+def main_app():
+    try:
+        search_value = request.args.get('keyword')
+        response = requests.get('http://localhost:5001/api/v1/term/' + search_value)
+        return render_template('index.html', response=response.json())
+    except:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
